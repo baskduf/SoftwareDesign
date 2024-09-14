0459ec5f06f3be7553449771bb25e24c6c66bcf1
baskduf <baskduf@ynu.ac.kr>
2024-09-14 17:11:09 +0900

    add : view /login/

--- softwaredesign/main/forms.py
+++ softwaredesign/main/forms.py
@@ -10,3 +10,14 @@ class SignupForm(forms.Form):
         max_length=256,
         widget=forms.PasswordInput(attrs={'class':'hero-form-input w-input', 'placeholder': 'Enter your password'}),label=''
     )
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        max_length=256,
+        widget=forms.TextInput(attrs={'class':'hero-form-input w-input', 'placeholder': 'Enter your email'}),label=''
+    )
+    password = forms.CharField(
+        max_length=256,
+        widget=forms.PasswordInput(attrs={'class':'hero-form-input w-input', 'placeholder': 'Enter your password'}),label=''
+    )
--- softwaredesign/main/views.py
+++ softwaredesign/main/views.py
@@ -17,7 +17,7 @@ def index(request):
 # myapp/views.py
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import SignupForm
+from .forms import SignupForm, LoginForm
 from django.contrib.auth.models import User
 
 def signup(request):
@@ -39,3 +39,29 @@ def signup(request):
         form = SignupForm()
     
     return render(request, 'signup.html', {'form': form})
+
+from django.contrib.auth import authenticate, login as auth_login
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            
+            # 사용자 인증
+            user = authenticate(request, username=email, password=password)
+            
+            if user is not None:
+                # 로그인 처리
+                auth_login(request, user)
+                messages.success(request, 'You are now logged in!')
+                return redirect('/')  # 로그인 후 리다이렉트할 페이지를 설정합니다.
+            else:
+                messages.error(request, 'Invalid email or password.')
+        else:
+            messages.error(request, 'There was an error with your submission.')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'login.html', {'form': form})
