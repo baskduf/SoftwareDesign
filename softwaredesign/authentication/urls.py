4733067df7d21639fef1217e33497f8a27f333c9
baskduf <baskduf@ynu.ac.kr>
2024-09-16 16:40:09 +0900

    add : create auth app

--- softwaredesign/authentication/urls.py
+++ softwaredesign/authentication/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from authentication import views
 
 urlpatterns = [
-    path('', include('main.urls')),
-    path('auth/', include('authentication.urls')),
-    path("admin/", admin.site.urls),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup')
 ]
