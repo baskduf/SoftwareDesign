03258ad008b5552862a48fe6f908213b39038d79
baskduf <baskduf@ynu.ac.kr>
2024-09-17 10:50:45 +0900

    add : aiservice app

--- softwaredesign/softwaredesign/settings.py
+++ softwaredesign/softwaredesign/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "main","authentication","adminapp"
+    "main","authentication","adminapp","aiservice"
 ]
 
 MIDDLEWARE = [
