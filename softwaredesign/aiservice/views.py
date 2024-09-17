8508d5a6b5a5e80c16832550716d8a06e9fab550
baskduf <baskduf@ynu.ac.kr>
2024-09-17 11:45:18 +0900

    add : chat layout

--- softwaredesign/aiservice/views.py
+++ softwaredesign/aiservice/views.py
@@ -15,7 +15,7 @@ def chat(request, ainame):
     ai = get_object_or_404(AI, ainame=ainame)
     image_url = ai.get_image_url()  # 이미지 경로 생성
 
-    return render(request, 'ai-index.html', {
+    return render(request, 'ai-chat.html', {
         'ai': ai,
         'image_url': image_url  # 이미지 경로 템플릿에 전달
     })
