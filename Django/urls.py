3f7c7a43d23d81382d405f3a789d135b53a3dbae
whdqja3640 <59163367+whdqja3640@users.noreply.github.com>
2024-09-18 23:45:35 +0900

    login

--- Django/urls.py
+++ Django/urls.py
@@ -5,7 +5,6 @@ from start import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
     path('board/', views.board_list, name='board_list'),
     path('board/create/', views.create_post, name='create_post'),
     path('', views.home, name='home'),  # 홈 페이지를 로그인 페이지로 렌더링
