71b19691f617b5401559df9b741afe29a54ee545
서준우 <csjws@naver.com>
2024-11-21 20:05:54 +0900

    update: 추천기능, about 페이지 디자인 완료

--- mygallery/gallery/static/gallery/scripts.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('moreBtn').addEventListener('click', function() {
-  const hiddenImages = document.querySelectorAll('.hidden');
-  
-  hiddenImages.forEach(image => {
-    image.classList.remove('hidden');
-  });
-
-  this.style.display = 'none'; // 버튼 숨기기
-});
--- mygallery/gallery/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Image
-
-def image_gallery(request):
-    images = Image.objects.all()
-    return render(request, 'gallery/image_gallery.html', {'images': images})
